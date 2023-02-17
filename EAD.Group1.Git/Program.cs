0ec7327aee3ee809315ee2793b85447c8511f5b3
Your Name <GitHub Email>
2023-02-16 19:25:39 -0500

    Added name prompt

--- EAD.Group1.Git/Program.cs
+++ EAD.Group1.Git/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello My Group.");
+
+// I need to write the first few lines
+var name = Console.ReadLine();
+
+Console.WriteLine($"My name is: {name}");
